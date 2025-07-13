fd6a004a3a4c2349262283b663cad964acad5eff
akovr <akovr@yahoo.com>
2025-07-14 05:48:00 +0700

    исправлено значение amount для OverLimit

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -56,9 +56,9 @@ public class BonusServiceTest {
         BonusService service = new BonusService();
 
         // подготавливаем данные:
-        long amount = 1000;
+        long amount = 1_000_000;
         boolean registered = false;
-        long expected = 10;
+        long expected = 500;
 
         // вызываем целевой метод:
         long actual = service.calculate(amount, registered);
